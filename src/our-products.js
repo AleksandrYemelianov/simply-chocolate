f526e7c6083474adfaa0aea08e8f302be5e2bffa
Daria Ishchenko <ishchenko1996work@gmail.com>
2023-03-18 17:31:16 +0200

    our-products update

---
+++ src/our-products.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+
+  loop: true,
+
+  slidesPerView: 'auto',
+
+  spaceBetween: 18,
+
+  freeMode: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
