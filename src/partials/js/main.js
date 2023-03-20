cc8aaf4ba2cdd09ab9cc56ed278e085b37809614
dasistsupercharger <hardyhard833@gmail.com>
2023-03-20 14:31:20 +0200

    modals buy & rewiev

---
+++ src/partials/js/main.js
@@ -0,0 +1,6 @@
+import './products-slider-click';
+import './header-menu';
+import './modal';
+import './review-slider';
+import './open-modal';
+import './tel-modal-review';
---
+++ src/partials/js/modal.js
@@ -0,0 +1,34 @@
+(function() {
+  "use strict";
+
+  const backdrop = document.querySelector('#modal-backdrop');
+  document.addEventListener('click', modalHandler);
+
+  function modalHandler(evt) {
+    const modalBtnOpen = evt.target.closest('.js-modal');
+    if (modalBtnOpen) { // open btn click
+      const modalSelector = modalBtnOpen.dataset.modal;
+      showModal(document.querySelector(modalSelector));
+    }
+
+    const modalBtnClose = evt.target.closest('.modal-close');
+    if (modalBtnClose) { // close btn click
+      evt.preventDefault();
+      hideModal(modalBtnClose.closest('.modal-window'));
+    }
+
+    if (evt.target.matches('#modal-backdrop')) { // backdrop click
+      hideModal(document.querySelector('.modal-window.show'));
+    }
+  }
+
+  function showModal(modalElem) {
+    modalElem.classList.add('show');
+    backdrop.classList.remove('hidden');
+  }
+
+  function hideModal(modalElem) {
+    modalElem.classList.remove('show');
+    backdrop.classList.add('hidden');
+  }
+})();
