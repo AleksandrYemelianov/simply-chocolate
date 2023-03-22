fb08d22e667e8c8308423802c225e63f2fa17f86
Alinapikalova <alina.pikalova@gmail.com>
2023-03-22 12:54:42 +0100

    add padding-bottom? del height
    fixed js, fixed css, and little del unneeded class from html

--- src/js/script-swiper.js
+++ src/js/script-swiper.js
@@ -1,20 +1,22 @@
 new Swiper('.reviews-swiper', {
+  direction: 'horizontal',
+  autoHeight: true,
+  loop: true,
+  slidesPerView: 'auto',
+
+  spaceBetween: 16,
+
+  freeMode: true,
+
   pagination: {
     el: '.swiper-pagination-reviews',
     clickable: true,
   },
+  autoHeight: true,
   breakpoints: {
-    // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 16,
     },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 16,
-    },
-    // when window width is >= 640px
     768: {
       slidesPerView: 2,
       spaceBetween: 16,
